79f9b46e097099268fa20c0c454f9b65cb5af283
Bennett <bennett.tai@presslogic.com>
2024-04-13 21:51:03 +0800

    add masking

--- masking.py
+++ masking.py
@@ -7,9 +7,29 @@ cv.imshow("Dog", img)
 blank = np.zeros(img.shape[:2], dtype="uint8")
 cv.imshow("Blank Image", blank)
 
-mask = cv.circle(blank, (img.shape[1] // 2, img.shape[0] // 2), 100, 255, -1)
-cv.imshow("Mask", mask)
+circle = cv.circle(
+    blank.copy(),
+    (img.shape[1] // 2, img.shape[0] // 2),
+    240,
+    255,
+    -1,
+)
+cv.imshow("Mask Circle", circle)
 
+rectangle = cv.rectangle(
+    blank.copy(),
+    (50, 50),
+    (679, 436),    
+    255,
+    -1,
+)
+cv.imshow("Mask Rectangle", rectangle)
+
+weird_shape = cv.bitwise_and(circle, rectangle)
+cv.imshow("Weird Shape", weird_shape)
+
+masked = cv.bitwise_and(img, img, mask=weird_shape)
+cv.imshow("Masked Image", masked)
 while True:
     if cv.waitKey(20) & 0xFF == ord("q"):
         break
