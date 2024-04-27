f3dc50c9ade64eff72f6863b7d71a395358c88ad
Bennett <bennett.tai@presslogic.com>
2024-04-27 22:27:21 +0800

    add face recognition

---
+++ face_recognition.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import numpy as np
+
+harr_cascade = cv.CascadeClassifier("haar_face.xml")
+
+people = ["Ben Afflek", "Elton John", "Jerry Seinfield", "Madonna", "Mindy Kaling"]
+# features = np.load("features.npy",allow_pickle=True)
+# labels = np.load("labels.npy",allow_pickle=True)
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.read("face_trained.yml")
+
+img = cv.imread(r"faces\val\elton_john\5.jpg")
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Person", gray)
+
+# Detect the face in the image
+faces_rect = harr_cascade.detectMultiScale(gray, 1.1, 4)
+
+for x, y, w, h in faces_rect:
+    faces_roi = gray[y : y + h, x : x + h]
+
+    label, confidence = face_recognizer.predict(faces_roi)
+    print(f"Label ={people} with a confidence of {confidence}")
+
+    cv.putText(
+        img,
+        str(people[label]),
+        (20, 20),
+        cv.FONT_HERSHEY_COMPLEX,
+        1.0,
+        (0, 255, 0),
+        thickness=2,
+    )
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+
+cv.imshow("Detected Face", img)
+
+while True:
+    if cv.waitKey(20) & 0xFF == ord("q"):
+        break
